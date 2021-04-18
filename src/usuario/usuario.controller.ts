856860a5ccb7d624c2465228a1659cad3ad020ac
Miguel <aquin9724@gmail.com>
2021-04-18 16:24:50 -0500

    CRUD modificado

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateUsuarioDTO } from './dtos/usuario.dto';
+import { UsuarioService } from './usuario.service';
 
 @Controller('usuario')
-export class UsuarioController {}
+export class UsuarioController {
+  constructor(private usuarioService: UsuarioService) {}
+
+  @Get()
+  async getUsuarios() {
+    const usuarios = await this.usuarioService.getUsuarios();
+    return {
+      usuarios,
+    };
+  }
+
+  @Post('create')
+  async create(@Body() dto: CreateUsuarioDTO) {
+    const create = await this.usuarioService.create(dto);
+    return {
+      message: 'CREADO CON EXITO',
+      create,
+    };
+  }
+
+  @Get(':id')
+  async getUsuario(@Param('id') id: string) {
+    const usuario = await this.usuarioService.getUsuario(id);
+    return { usuario };
+  }
+
+  @Put(':id')
+  async edited(@Param('id') id: number, @Body() edit: CreateUsuarioDTO) {
+    const edited = await this.usuarioService.editUsuario(id, edit);
+    return { message: 'EDITADO CON EXITO', edited };
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number) {
+    const deleted = await this.usuarioService.Deleted(id);
+    return { message: 'ELIMINADO CON EXITO', deleted };
+  }
+}
