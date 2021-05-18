10ab6db05d8e077e8978697eb388f83a53d82634
Miguel <aquin9724@gmail.com>
2021-05-18 09:38:57 -0500

    actualizar backen

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,7 +2,9 @@ import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
+import { PropietarioDecorator } from './auth/decorator/propietario.decorator';
 import { LoginDto } from './auth/dto/login.dto';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/guards/local-auth.guard';
 
 @Controller()
@@ -12,9 +14,16 @@ export class AppController {
     private authService: AuthService,
   ) {}
 
+  @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Body() login: LoginDto) {
     const { correo, password } = login;
     return await this.authService.login(correo);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@PropietarioDecorator() propietario: any) {
+    return propietario;
   }
 }
